a58c870ff5b97f02334bb10373c52c18003b123d
LordMefloun <realfrog42@gmail.com>
2021-09-12 19:47:50 +0200

    IdeaProject

---
+++ src/main/java/me/lordmefloun/vanish/Vanish.java
@@ -0,0 +1,17 @@
+package me.lordmefloun.vanish;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Vanish extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("kekw");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
